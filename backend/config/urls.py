352a74af0058d791f960f3f41cb3339fcae0748d
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-19 23:35:25 -0300

    Ajustes 19-04

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from django.http import HttpResponse
 
 from django.conf.urls.static import static
 
@@ -19,6 +19,7 @@ urlpatterns = [
     path('pet_registration/', include('apps.pet_registration.urls')),
     path('search/', include('apps.search.urls')),
     path("healthz", health_check),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

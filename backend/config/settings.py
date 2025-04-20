355325758d2452d821a54681198c04e78b2d23d0
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-19 23:08:23 -0300

    Ajustes 29-04 Chat pronto

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Segurança
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 # Token de IP
 IPINFO_TOKEN = os.getenv('IPINFO_TOKEN')

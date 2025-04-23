0344d529f9ece616e5f8a9ee68a5b3a20a89c02b
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-23 19:10:14 -0300

    Ajustes 23-04

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -219,11 +219,11 @@ if not all([os.getenv('CLOUD_NAME'), os.getenv('CLOUD_API_KEY'), os.getenv('CLOU
     raise Exception("⚠️ Variáveis do Cloudinary não configuradas corretamente.")
 
 # Cloudinary
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUD_API_KEY'),
     'API_SECRET': os.getenv('CLOUD_API_SECRET'),
 }
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 

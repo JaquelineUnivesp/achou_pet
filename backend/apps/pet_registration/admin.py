e2b011bcf13172fafdcb5b8e6c3242c193a06289
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-23 18:54:22 -0300

    Ajustes 23-04

--- backend/apps/pet_registration/admin.py
+++ backend/apps/pet_registration/admin.py
@@ -2,3 +2,11 @@ from apps.pet_registration.models import LostPet
 
 # Veja TODOS os pets cadastrados no banco de dados
 LostPet.objects.all()
+
+
+from django.contrib import admin
+from .models import LostPet, PetAdoption, BreedingPet  # ou o que você tiver
+
+admin.site.register(LostPet)
+admin.site.register(PetAdoption)
+admin.site.register(BreedingPet)

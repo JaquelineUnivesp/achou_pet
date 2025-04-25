825724311d3551150e4d90845c80d6731d0deb6b
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-24 21:38:59 -0300

    Ajustes 24-04

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -2,37 +2,31 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = (
-        'email', 'username', 'first_name', 'last_name', 'is_staff',
-        'is_verified', 'notification_phone', 'cpf'
-    )
-    fieldsets = UserAdmin.fieldsets + (
-        ('Informações Adicionais', {
-            'fields': (
-                'notification_phone',
-                'photo',
-                'cpf',
-                'notify_chat',
-                'notify_pet_found',
-                'notify_other',
-                'is_verified',
-            )
-        }),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Informações Adicionais', {
-            'fields': (
-                'notification_phone',
-                'photo',
-                'cpf',
-                'notify_chat',
-                'notify_pet_found',
-                'notify_other',
-                'is_verified',
-            )
-        }),
+    list_display = ('email', 'username', 'is_staff', 'is_active', 'is_verified')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'is_verified')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'cpf', 'photo', 'notification_phone')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+        ('Notificações', {'fields': ('notify_chat', 'notify_pet_found', 'notify_other')}),
+        ('Verificação', {'fields': ('is_verified',)}),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'username', 'password1', 'password2',
+                'is_staff', 'is_active', 'is_superuser',
+                'notification_phone', 'cpf', 'photo',
+                'notify_chat', 'notify_pet_found', 'notify_other', 'is_verified'
+            )
+        }),
+    )

fa6e66a95d990b8f0525831a7622a8ace7243f7e
jaquelinedamian <2230219@aluno.univesp.br>
2025-04-28 21:55:37 -0300

    Ajustes 28-04 - views

--- backend/apps/pet_registration/urls.py
+++ backend/apps/pet_registration/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import user_pets_list, pet_detail, edit_pet, delete_pet, lost_pet_detail
+from .views import user_pets_list, pet_detail, edit_pet, delete_pet
 
 app_name = 'pet_registration'
 
@@ -18,13 +18,10 @@ urlpatterns = [
     # Pets do usuário
     path('pets_user/', views.user_pets_list, name='user_pets_list'),
 
-    # Pets Perdidos (Privado)
+    # Pets Perdidos
     path('pet/<int:pet_id>/', pet_detail, name='pet_detail'),
     path('pet/<int:pet_id>/editar/', edit_pet, name='edit_pet'),
     path('pet/<int:pet_id>/deletar/', delete_pet, name='delete_pet'),
-
-    # Pets Perdidos (Público - Novo!)
-    path('lost_pet/<int:pet_id>/', views.lost_pet_detail, name='lost_pet_detail'),
 
     # Pets para adoção
     path('adoption_pet/<int:pet_id>/', views.adoption_pet_detail, name='adoption_pet_detail'),

4b4ca373ffdca2d451bb6a4ffe181ab291489c34
jaquelinedamian <2230219@aluno.univesp.br>
2025-05-10 21:34:15 -0300

    Ajustes pagina de busca

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -169,19 +169,27 @@ REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 REDIS_DB = int(os.getenv('REDIS_DB', 0))
 
+
+
+# Verificar se está em produção (Render)
+REDIS_URL = os.getenv('REDIS_URL')
+
+if not REDIS_URL:
+    raise Exception("⚠️ A variável REDIS_URL não está configurada corretamente.")
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(REDIS_HOST, REDIS_PORT)],
+            "hosts": [REDIS_URL],
         },
     },
 }
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/1',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
